1d353f2484e538da58c699d6ed40cc29b7f29fb8
keckoss13 <73256718+keckoss13@users.noreply.github.com>
2023-06-17 16:24:32 +0200

    update

---
+++ backend/serveur.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const formData = require("form-data");
+const Mailgun = require("mailgun.js");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+/* MAILGUN CONFIGURATION */
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({
+  username: "John Doe",
+  key: process.env.APIKEY /* VOTRE CLÉ API */,
+});
+
+app.get("/", (req, res) => {
+  res.send("server is up");
+});
+
+app.post("/form", async (req, res) => {
+  try {
+    //   Le console.log de req.body nous affiche les données qui ont été rentrées dans les inputs (dans le formulaire frontend) :
+    console.log(req.body);
+
+    // On destructure req.body
+    const { firstname, lastname, email, message } = req.body;
+
+    //   On crée un objet messageData qui contient des informations concernant le mail (qui m'envoie le mail, adresse vers laquelle je veux envoyer le mail, titre et contenu du mail) :
+    const messageData = {
+      from: `${firstname} ${lastname} <${email}>`,
+      to: process.env.RECEIPT,
+      subject: `Formulaire JS`,
+      text: message,
+    };
+
+    // On envoie les infos à Mailgun pour créer le mail et l'envoyer
+    const response = await client.messages.create(
+      process.env.SANDBOX, // <==== ⚠️ le domain commence par "sandbox" et fini par ".mailgun.org"
+      messageData
+    );
+
+    console.log(response);
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server is listening");
+});
---
+++ script.js
@@ -0,0 +1,41 @@
+// gestion de la modale
+
+function openModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  var modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+// gestion de l'envoi des mails
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded");
+
+  document
+    .querySelector("#contactform")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+      };
+
+      console.log(data);
+
+      const response = await axios.post("http://localhost:3000/form", data);
+      console.log(response);
+    });
+});
